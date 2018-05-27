f46e2fc845009e1ecb80a896f2a563fb85ee5ab4
mambrozio <mambrozio@users.noreply.github.com>
2018-05-26 21:14:09 -0300

    Part 1 modified to correct issues.

--- Assignment-03/teste.lua
+++ Assignment-03/teste.lua
@@ -19,8 +19,9 @@ while not(exit) do
 		nx_uint16_t type;
 		nx_uint16_t source;
 		nx_uint16_t target;
-		nx_uint16_t node;
-		nx_uint16_t temp;
+		nx_uint16_t ushort_01;
+		nx_uint16_t ushort_02;
+		nx_uint16_t ushort_03;
 	  }; 
 	]]
 
@@ -29,11 +30,11 @@ while not(exit) do
 		local stat, msg, emsg = pcall(function() return mote:receive() end) 
 		--print(stat, msg, emsg)
 		if stat then
-			if msg then
+			if msg and msg.type == 1280 then
 				print("------------------------------") 
 				print("msgID: "..msg.type, "Source: ".. msg.source, "Target: ".. msg.target) 
-				print("n:", msg.node)
-				print("t:", msg.temp)
+				print("n:", msg.ushort_01)
+				print("t:", msg.ushort_02)
 			else
 				if emsg == "closed" then
 					print("\nConnection closed!")
